38b5a20c70df4b19f6478ab66f9be340ba36c986
Weidaicheng <wdcdavyc@outlook.com>
2020-06-26 19:00:15 +0800

    add logging for ErrorController

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using WebApi.Template.Models;
 
 namespace WebApi.Template.Controllers
@@ -7,10 +8,19 @@ namespace WebApi.Template.Controllers
     [ApiController]
     public class ErrorController : ControllerBase
     {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         [Route("api/Error")]
         public Result<string> Error(string message)
         {
+            _logger.LogError(message);
+
             return new Result<string>(HttpStatusCode.InternalServerError, message);
         }
     }
